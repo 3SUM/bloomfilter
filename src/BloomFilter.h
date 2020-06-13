b83c4d05e172ec91644c792fbb9f7a1b9a299c4c
LuisMaya <mayafluis@gmail.com>
2020-06-12 17:04:18 -0700

    Refractoring repo

---
+++ src/BloomFilter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace kitana
+{
+    template <typename T>
+    class BloomFilter
+    {
+    private:
+    public:
+    };
+} // namespace kitana
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "BloomFilter.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "test" << std::endl;
+    return 0;
+}./

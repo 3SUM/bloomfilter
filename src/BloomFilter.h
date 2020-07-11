51ba4c5f77c23bf0631fda0a12f2f4d49cc15401
LuisMaya <mayafluis@gmail.com>
2020-07-10 20:14:37 -0700

    Bloomfilter implementation

--- src/BloomFilter.h
+++ src/BloomFilter.h
@@ -1,11 +1,74 @@
 #pragma once
 
+#include "MurmurHash3.h"
+
+#include <array>
+#include <bitset>
+#include <cstdint>
+#include <iostream>
+
 namespace kitana
 {
     template <typename T>
-    class BloomFilter
+    class bloomfilter
     {
     private:
+        // Expected number of elements n
+        static constexpr uint64_t n = 256;
+
+        // Size of bit array m
+        // Calculated by: m = -((nlnp)/(ln2)^2)
+        static constexpr uint64_t m = 1596;
+
+        // Number of hash functions k
+        // Calculated by: k = (m/n)ln2
+        static constexpr uint8_t k = 4;
+
+        // Probability of false positive p
+        static constexpr float p = 0.05;
+
+        // Bloom filter
+        std::bitset<m> filter;
+
     public:
+        bloomfilter() = default;
+        ~bloomfilter() = default;
+
+        std::array<uint64_t, 2> hash(const T *key)
+        {
+            std::array<uint64_t, 2> hashes;
+            MurmurHash3_x64_128(key, sizeof(key), 0, hashes.data());
+            return hashes;
+        }
+
+        inline uint64_t nthhash(uint8_t n, uint64_t hashA, uint64_t hashB)
+        {
+            return (hashA + n + hashB) % m;
+        }
+
+        void add(const T *key)
+        {
+            auto hashes = hash(key);
+
+            for (uint8_t n = 0; n < k; n++)
+            {
+                filter[nthhash(n, hashes[0], hashes[1])] = true;
+            }
+        }
+
+        bool query(const T *key)
+        {
+            auto hashes = hash(key);
+
+            for (uint8_t n = 0; n < k; n++)
+            {
+                if (!filter[nthhash(n, hashes[0], hashes[1])])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     };
 } // namespace kitana
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,73 @@
-#include "BloomFilter.h"
-#include <iostream>
+#include "bloomfilter.h"
+
+#include <fstream>
+#include <string>
+
+#include <cstdio>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+void handle_error(const char *msg)
+{
+    perror(msg);
+    exit(255);
+}
+
+static uintmax_t stdread(const std::string fname)
+{
+    std::string temp;
+    std::ifstream input;
+    uintmax_t lines = 0;
+
+    input.open(fname.c_str());
+    if (input.is_open())
+    {
+        input >> temp;
+
+        while (input)
+        {
+            ++lines;
+            input >> temp;
+        }
+        input.close(); 
+    }
+    else {
+        handle_error("open");
+    }
+
+    return lines;
+}
+
+static uintmax_t wc(char const *fname)
+{
+    static const auto BUFFER_SIZE = 16 * 1024;
+    int fd = open(fname, O_RDONLY);
+    if (fd == -1)
+        handle_error("open");
+
+    char buf[BUFFER_SIZE + 1];
+    uintmax_t lines = 0;
+
+    while (size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+    {
+        if (bytes_read == (size_t)-1)
+            handle_error("read failed");
+        if (!bytes_read)
+            break;
+
+        for (char *p = buf; (p = (char *)memchr(p, '\n', (buf + bytes_read) - p)); ++p)
+        {
+            ++lines;
+        }
+    }
+
+    return lines;
+}
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    //printf("Lines: %lu\n", wc("src/5million.csv"));
+    printf("Lines: %lu\n", stdread("src/5million.csv"));
     return 0;
-}./
+}
